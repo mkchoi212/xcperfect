2dd9afdfa91db0d3a797df0a8e7223834bdbb08d
Mike JS. Choi <mkchoi212@icloud.com>
2018-06-02 13:49:43 -0500

    Print all files in target

--- lib/xcperfect/formatters/all.rb
+++ lib/xcperfect/formatters/all.rb
@@ -1,16 +1,24 @@
 module XCPerfect
+  # All prints all code coverage information provided by xccov
   class All < Formatter
-    def pretty_format(targets)
-      @parser.extract(targets).each do |target|
-        puts pretty_details(target)
-        puts @parser.coverage_percentage(target)
-      end
+    DOWN = '▼'.freeze
+    DOWN_ASCII = '->'.freeze
 
-      puts @parser.coverage_percentage(@parser.json)
+    def pretty_name(target)
+      symbol = yellow(@use_ascii ? DOWN_ASCII : DOWN)
+      [symbol, white(target['name'])].join(' ')
     end
 
-    def pretty_details(target)
-      puts target.keys
+    def pretty_files(target)
+      target['files'].map do |file|
+        "\t" + pretty_name(file)
+      end.join("\n")
+    end
+
+    def pretty_format(targets)
+      @parser.extract(targets).map do |target|
+        [pretty_name(target), pretty_files(target)]
+      end.join("\n")
     end
   end
 end
