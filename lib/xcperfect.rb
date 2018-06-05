4ff245c330be383ed0cb3ac2b62f29260e6e5e3c
Mike JS. Choi <mkchoi212@icloud.com>
2018-06-05 00:27:39 -0700

    Display appropriate warnings for fatal events

--- lib/xcperfect.rb
+++ lib/xcperfect.rb
@@ -8,10 +8,13 @@ require 'xcperfect/formatters/formatter'
 require 'xcperfect/formatters/all'
 require 'xcperfect/formatters/simple'
 
+include XCPerfect::ANSI
+
 # XCPerfect contains class level functions
 module XCPerfect
-  def self.exit_with_error(message)
-    $stderr.puts "[!] #{message}"
+  def self.exit_with_error(message, colorize)
+    @colorize = colorize
+    $stderr.puts red("xcperfect: #{message}")
     exit 1
   end
 end
